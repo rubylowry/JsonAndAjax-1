3f70cf19d2d8e30172ca622d3d7f535b75ace627
beulathamban <beulathamban@gmail.com>
2020-02-11 13:50:35 +1300

    json external file

---
+++ js/script.js
@@ -0,0 +1,67 @@
+console.log('json & ajax'); //testing script.js
+
+
+//to test jquery
+ $(document).ready(function(){
+   $('body').click(function(){
+    $(this).css('color','blue');
+   });
+ });
+
+// Javascript data - array of objects
+var jsData = [
+  {
+    name : "Peter",
+    age  : 50
+  },
+  {
+    name : "John",
+      age  : 25
+  }
+
+];
+
+console.log(jsData);
+
+//displaying js objects in the browser window
+var i;
+for(i =0; i < jsData.length; i++) {
+document.getElementById('result').innerHTML +=
+    '<h1>' + jsData[i].name +' is ' +
+     jsData[i].age + ' years old</h1>';
+};
+
+
+//json data
+var jsonData = [
+  {
+    "name" : "Lucy",
+    "age" : 12
+  },
+  {
+    "name" : "Mark",
+      "age" : 77
+  }
+];
+
+console.log(jsonData);
+
+//displaying json objects in the browser window
+var j;
+for(j =0; j < jsonData.length; j++) {
+document.getElementById('result').innerHTML +=
+    '<h1>' + jsonData[j].name +' is ' +
+     jsonData[j].age + ' years old</h1>';
+};
+
+// retrieving json data from an external file
+var person = JSON.parse(myData);
+console.log(person);
+
+//displaying json objects in the browser window
+var k;
+for(k =0; k < person.length; k++) {
+document.getElementById('result').innerHTML +=
+    '<h1>' + person[k].name +' is ' +
+     person[k].age + ' years old</h1>';
+};
