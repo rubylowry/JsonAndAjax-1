2035fb07298a4357f0bb9e59fe56ce0a691dcfc4
Beula Samuel <092834@bun2092834.local>
2020-02-14 10:24:37 +1300

    added users endpoint

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log('json & ajax'); //testing script.js
 
 
 //to test jquery
- $(document).ready(function(){
+$(document).ready(function(){
    // $('body').click(function(){
    //  $(this).css('color','blue');
 
@@ -17,7 +17,8 @@ console.log(myKey);
 	endPoint = document.getElementById('endpoints').value;
 	size = document.getElementById('sizes').value;
   console.log(endPoint,size); //actual parameter
-  displayData(endPoint,size);//actual parameter
+  var apiUser ='harleydavidson';
+  displayData(endPoint, size, apiUser);//actual parameter
   });
 
 // Javascript data - array of objects
@@ -78,11 +79,17 @@ console.log(myKey);
 //      person[k].age + ' years old</h1>';
 // };
 
-function displayData(ep, si){
-  console.log(ep, si);
+function displayData(ep, si, au){
+  if (ep === 'users') {
+  var url = `https://api.unsplash.com/users/${au}/?client_id=${myKey}`;
+  } else {
+   var url =  `https://api.unsplash.com/${ep}/?client_id=${myKey}`;
+  }
+  console.log(ep, si, url);
+
 //ajax method
 $.ajax({
-  url:`https://api.unsplash.com/${ep}/?client_id=${myKey}`,
+  url:url,
   type:'GET',
   data:'json',
   success: function(data){
@@ -91,73 +98,130 @@ $.ajax({
       collections(data,ep, si);
     } else if (ep === 'photos'){
       photos(data, ep, si);
+    } else if (ep === 'users') {
+      users(data, ep, si);
     }
-    function collections(d, e,s){
-      var k;
-      var userSize;
+
+      //collections
+      function collections(d, e,s){
+        var k;
+        var userSize;
 
         document.getElementById('result').innerHTML = '';
-      for(k = 0; k < d.length; k++ ){
-        if (s === 'full') {
-          userSize = d[k].cover_photo.urls.full;
-        } else if (s === 'raw') {
-          userSize = d[k].cover_photo.urls.raw;
-        } else if (s === 'regular') {
-          userSize = d[k].cover_photo.urls.regular;
-        }else if (s === 'small') {
-          userSize = d[k].cover_photo.urls.small;
-        } else if (s === 'thumb') {
-          userSize = d[k].cover_photo.urls.thumb;
-        }
+
+        for(k = 0; k < d.length; k++ ){
+          if (s === 'full') {
+            userSize = d[k].cover_photo.urls.full;
+          } else if (s === 'raw') {
+            userSize = d[k].cover_photo.urls.raw;
+          } else if (s === 'regular') {
+            userSize = d[k].cover_photo.urls.regular;
+          }else if (s === 'small') {
+            userSize = d[k].cover_photo.urls.small;
+          } else if (s === 'thumb') {
+            userSize = d[k].cover_photo.urls.thumb;
+          }
 
 
-        document.getElementById('result').innerHTML +=
-        '<div class="col">' +
-          '<img class="img-thumbnail" alt="Image" src="' + userSize + '">' +
-        '</div>';
-      }
-    }
+          document.getElementById('result').innerHTML +=
+          '<div class="col-6 my-3">' +
+
+            '<img class="img-thumbnail" alt="Image" src="' + userSize + '">' +
+          '</div>' +
+          '<div class="col-6 my-3">' +
+
+          '<h1 class="my2">' + d[k].title + ' ' + d[k].user.first_name +'</h1> <h2 class="my-3">' + 
+          d[k].description + '<br><br> Published at: ' + d[k].published_at + '</h2> <h3> ' + d[k].user.bio +' </h3>' +
+            
+          '</div>';
+        }//for
+      }//collections function
 
 
-      function photos(d, e,s){
+
+      //Photos
+        function photos(d, e,s){
           var j;
-        var photoSize;
+          var photoSize;
           document.getElementById('result').innerHTML = '';
-      for(j = 0; j < d.length; j++ ){
-        if (s === 'full') {
-          photoSize = d[j].urls.full;
-        } else if (s === 'raw') {
-          photoSize = d[j].urls.raw;
-        } else if (s === 'regular') {
-          photoSize = d[j].urls.regular;
-        }else if (s === 'small') {
-          photoSize = d[j].urls.small;
-        } else if (s === 'thumb') {
-          photoSize = d[j].urls.thumb;
-        }
+
+          for(j = 0; j < d.length; j++ ){
+            if (s === 'full') {
+              photoSize = d[j].urls.full;
+            } else if (s === 'raw') {
+              photoSize = d[j].urls.raw;
+            } else if (s === 'regular') {
+              photoSize = d[j].urls.regular;
+            }else if (s === 'small') {
+              photoSize = d[j].urls.small;
+            } else if (s === 'thumb') {
+              photoSize = d[j].urls.thumb;
+            }
 
 
-        document.getElementById('result').innerHTML +=
-        '<div class="col">' +
-          '<img class="img-thumbnail" alt="Image" src="' + photoSize + '">' +
-        '</div>';
-      }
-    }
+          document.getElementById('result').innerHTML +=
+          '<div class="col-6">' +
+            '<img class="img-thumbnail" alt="Image" src="' + photoSize + '">' +
+          '</div>';
+        } //for
+      }//photos function
+
+
+      //Users
+      function users(d, e, s) {
+        console.log(d,e,s)
+        document.getElementById('result').innerHTML = '';
+        var p;
+        var userPhoto;    
+        console.log(d.photos[0].urls.full);
+
+          
+          
+
+           document.getElementById('result').innerHTML +=  
+          '<div class="row border border-success">';
+
+          for(p = 0; p < d.photos.length; p++) {
+              if (s === 'full') {
+                userPhoto = d.photos[p].urls.full;
+              } else if (s === 'raw') {
+                userPhoto = d.photos[p].urls.raw;
+              } else if (s === 'regular') {
+                userPhoto = d.photos[p].urls.regular;
+              }else if (s === 'small') {
+                userPhoto = d.photos[p].urls.small;
+              } else if (s === 'thumb') {
+                userPhoto = d.photos[p].urls.thumb;
+              }
+              document.getElementById('result').innerHTML +=        
+             
+              '<img class="col-4 img-thumbnail" alt="Image" src="' + userPhoto + '">'          
+            
+           }//for
+               
+        
+
+         document.getElementById('result').innerHTML +=
+         '</div>'+
+          '<div class="mt-5 text-primary bg-info px-5 py-5">' +          
+            '<h1 class="my2">' + d.first_name + ' ' + d.last_name +'</h1> <h2 class="my-3">' + 
+            d.location + ' Total likes:' +d.total_likes + '</h2> <h3> ' + d.portfolio_url +' </h3>' +
+            '<img class="img-thumbnail" alt="Profile Image" src="' + d.profile_image.small + '">' +
+          '</div>'  + '<br>' ;
+
+      } //users function
 
 
 
 
 
 
+      },//success
+      error:function(){
+        console.log('error');
+      }//error
 
-
-
-  },//success
-  error:function(){
-    console.log('error');
-  }//error
-
- });//ajax
-}; // functione displayData ends here
+     });//ajax
+  }; // functione displayData ends here
 
 });//document.ready
