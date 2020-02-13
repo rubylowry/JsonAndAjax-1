1fcfd9873b08dd73bd294bbd9160c2f28ea311a1
beulathamban <beulathamban@gmail.com>
2020-02-13 15:18:08 +1300

    added endpoints for user's choice

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ console.log('json & ajax'); //testing script.js
 
 //to test jquery
  $(document).ready(function(){
-   $('body').click(function(){
-    $(this).css('color','blue');
-   });
+   // $('body').click(function(){
+   //  $(this).css('color','blue');
 
+    //accessing key from json file
+var myKey = JSON.parse(apiKey);
+console.log(myKey[0]);
+myKey = myKey[0].key;
+console.log(myKey);
+
+//reading users choice
+	document.getElementById('submit').addEventListener('click', function(){
+	endPoint = document.getElementById('endpoints').value;
+	size = document.getElementById('sizes').value;
+  console.log(endPoint,size); //actual parameter
+  displayData(endPoint,size);//actual parameter
+  });
 
 // Javascript data - array of objects
 // var jsData = [
@@ -66,31 +78,86 @@ console.log('json & ajax'); //testing script.js
 //      person[k].age + ' years old</h1>';
 // };
 
-
+function displayData(ep, si){
+  console.log(ep, si);
 //ajax method
 $.ajax({
-  url:'js/mock_data.json',
+  url:`https://api.unsplash.com/${ep}/?client_id=${myKey}`,
   type:'GET',
   data:'json',
   success: function(data){
     console.log(data);
-    var i;
-    for (i=0; i<data.length; i++){
-      document.getElementById('result').innerHTML +=
-      '<div class="col col-sm-6 col-md-6 col-lg-4 mx-2 my-5">' +
-      '<h1>' + data[i].first_name  +
-       data[i].last_name + '</h1>' +
-       '<h2>' + data[i].gender + '</h2>' +
-       '<h3>' + data[i].email + '<h3>' +
-       '<img class="img-thumbnail" src="' + data[i].avatar + '" alt="Avatar">' +
-      '</div>';
+    if (ep === 'collections'){
+      collections(data,ep, si);
+    } else if (ep === 'photos'){
+      photos(data, ep, si);
     }
+    function collections(d, e,s){
+      var k;
+      var userSize;
+
+        document.getElementById('result').innerHTML = '';
+      for(k = 0; k < d.length; k++ ){
+        if (s === 'full') {
+          userSize = d[k].cover_photo.urls.full;
+        } else if (s === 'raw') {
+          userSize = d[k].cover_photo.urls.raw;
+        } else if (s === 'regular') {
+          userSize = d[k].cover_photo.urls.regular;
+        }else if (s === 'small') {
+          userSize = d[k].cover_photo.urls.small;
+        } else if (s === 'thumb') {
+          userSize = d[k].cover_photo.urls.thumb;
+        }
+
+
+        document.getElementById('result').innerHTML +=
+        '<div class="col">' +
+          '<img class="img-thumbnail" alt="Image" src="' + userSize + '">' +
+        '</div>';
+      }
+    }
+
+
+      function photos(d, e,s){
+          var j;
+        var photoSize;
+          document.getElementById('result').innerHTML = '';
+      for(j = 0; j < d.length; j++ ){
+        if (s === 'full') {
+          photoSize = d[j].urls.full;
+        } else if (s === 'raw') {
+          photoSize = d[j].urls.raw;
+        } else if (s === 'regular') {
+          photoSize = d[j].urls.regular;
+        }else if (s === 'small') {
+          photoSize = d[j].urls.small;
+        } else if (s === 'thumb') {
+          photoSize = d[j].urls.thumb;
+        }
+
+
+        document.getElementById('result').innerHTML +=
+        '<div class="col">' +
+          '<img class="img-thumbnail" alt="Image" src="' + photoSize + '">' +
+        '</div>';
+      }
+    }
+
+
+
+
+
+
+
+
 
   },//success
   error:function(){
     console.log('error');
   }//error
 
-});//ajax
+ });//ajax
+}; // functione displayData ends here
 
 });//document.ready
